32e7417ca82ef601447fc1fd7dedbeb111f0b3b1
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-14 20:40:49 +0530

    storing and fetching movies from local storage

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ const average = arr =>
 const KEY = '8d89e8ca';
 
 export default function App() {
-  const [query, setQuery] = useState('Inception');
+  const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
   const [selectedId, setSelectedId] = useState(null);
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const stroedValue = JSON.parse(localStorage.getItem('watched'));
+    return stroedValue;
+  });
 
   function handleSelectMovie(id) {
     setSelectedId(selectedId => (id === selectedId ? null : id));
@@ -24,11 +28,20 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched(watched => [...watched, movie]);
+
+    // localStorage.setItem('watched', JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatch(id) {
     setWatched(watched => watched.filter(movie => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem('watched', JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
